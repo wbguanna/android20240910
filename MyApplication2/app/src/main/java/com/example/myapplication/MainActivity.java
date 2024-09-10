25c88cd6c989a48d16089cd8f0b17de8f55f4006
jihwan <80106655+Eguanna@users.noreply.github.com>
2024-09-10 11:30:37 +0900

    day32 Adapter 적용

--- MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,40 +1,45 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    // RecyclerView의 설정과 데이터 바인딩을 관리.
+    private RecyclerView recyclerView;
+    // RecyclerView에 표시될 아이템 데이터와 뷰를 연결.
+    private MyAdapter adapter;
+    // RecyclerView에 표시될 아이템의 데이터가 저장.
+    private List<String> itemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText editText = findViewById(R.id.editText);
-        Button button = findViewById(R.id.button);
-        Button gotoImgActivityBtn = findViewById(R.id.gotoImgActivityBtn);
+        // activity_main.xml에 정의한 RecyclerView
+        recyclerView = findViewById(R.id.recyclerView);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String text = editText.getText().toString();
-                Intent intent = new Intent(MainActivity.this, SubActivity.class);
-                intent.putExtra("inputText", text);
-                startActivity(intent);
-            }
-        });
+        // itemList 필드에 RecyclerView에 표시될 아이템 데이터를 초기화
+        // 나중에 실제로 ArrayList 객체를 new 연산자로 생성 해야 함.
+        itemList = Arrays.asList("Item 1", "Item 2", "Item 3", "Item 4");
 
-        gotoImgActivityBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ImageActivity.class);
-                startActivity(intent);
-            }
-        });
+        // MyAdapter 생성자에 itemList를 전달하여 어댑터가 사용할 데이터를 설정
+        adapter = new MyAdapter(itemList);
+        // RecyclerView의 레이아웃을 선형으로 설정
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        // setLayoutManager 메서드를 사용하여 RecyclerView에 레이아웃 매니저를 설정.
+        recyclerView.setLayoutManager(layoutManager);
+        // setAdapter 메서드를 사용하여 RecyclerView에 어댑터를 설정.
+        // 이로써 RecyclerView는 itemList 데이터를 사용하여 아이템을 표시하게 됨.
+        recyclerView.setAdapter(adapter);
     }
 }
